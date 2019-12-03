b9d069ba3f9ca36ee017f5df21f38c60da1f0c34
Edouard <edouard.p44@gmail.com>
2019-12-03 19:04:26 +0100

    Cart

---
+++ src/Controller/CartController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\TicketingRepository;
+use App\Service\Cart\CartManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("cart", name="cart_")
+ */
+class CartController extends AbstractController
+{
+    private $cartManager;
+
+    public function __construct(CartManager $cartManager)
+    {
+        $this->cartManager = $cartManager;
+    }
+
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+        return $this->render('cart/index.html.twig', [
+            'items' => $this->cartManager->getFullCart(),
+            'total' => $this->cartManager->getTotal()
+        ]);
+    }
+
+    /**
+     * @Route("/clean", name="clean")
+     */
+    public function cleanSession(SessionInterface $session): RedirectResponse
+    {
+        $session->clear();
+        return $this->redirectToRoute("cart_ticket");
+    }
+
+    /**
+     * @Route("/add/{id}", name="add")
+     */
+    public function addToCart($id): RedirectResponse
+    {
+        if ($this->isGranted('ROLE_USER')) {
+            $this->cartManager->add($id);
+            $this->addFlash('success',"Ca a bien été ajoutez à votre panier");
+        } else {
+            $this->addFlash('danger', 'Vous devez vous connecter pour acheter un billet');
+        }
+        return $this->redirectToRoute("cart_ticket");
+    }
+
+    /**
+     * @Route("/ticket", name="ticket")
+     */
+    public function ticketingList(TicketingRepository $ticketingRepository): Response
+    {
+        return $this->render('/cart/ticket.html.twig', [
+            'ticket' => $ticketingRepository->findAll(),
+        ]);
+
+    }
+
+    /**
+     * @Route("/remove/{id}", name="remove")
+     */
+    public function remove($id)
+    {
+        $this->cartManager->remove($id);
+        return $this->redirectToRoute('cart_index');
+    }
+}
---
+++ src/Service/Cart/CartManager.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Service\Cart;
+
+use App\Repository\TicketingRepository;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class CartManager
+{
+    private $session;
+
+    private $ticketingRepository;
+
+    public function __construct(SessionInterface $session, TicketingRepository $ticketingRepository)
+    {
+        $this->session = $session;
+        $this->ticketingRepository = $ticketingRepository;
+    }
+
+    public function add(int $id)
+    {
+        $cart = $this->session->get('cart', []);
+        if (!empty($cart[$id])) {
+            $cart[$id]++;
+        } else {
+            $cart[$id] = 1;
+        }
+        $this->session->set('cart', $cart);
+    }
+
+    public function remove(int $id)
+    {
+        $cart = $this->session->get('cart', []);
+        if (!empty($cart[$id])) {
+            unset($cart[$id]);
+        }
+        $this->session->set('cart', $cart);
+    }
+
+    public function getFullCart(): array
+    {
+        $cart = $this->session->get('cart', []);
+
+        $cartWithData = [];
+
+        foreach ($cart as $id => $quantity) {
+            $cartWithData[] = [
+                'ticket' => $this->ticketingRepository->find($id),
+                'quantity' => $quantity
+            ];
+        }
+        return $cartWithData;
+    }
+
+    public function getTotal(): float
+    {
+        $total = 0;
+        foreach ($this->getFullCart() as $item) {
+            $total += $item['ticket']->getPrice() * $item['quantity'];
+        }
+        return $total;
+
+    }
+
+}
